ba67374de089fdfbf1f93d1a08bee5f6176f425c
Helio Brito <helioh3@gmail.com>
2024-03-18 00:44:48 -0300

    feat: backend - paginate improvement to list

--- source/Infrastructure/Controllers/Operations/Index.php
+++ source/Infrastructure/Controllers/Operations/Index.php
@@ -3,12 +3,13 @@
 namespace Defi\Infrastructure\Controllers\Operations;
 
 use Defi\Infrastructure\GenericResource;
+use Illuminate\Http\Request;
 
 trait Index
 {
-    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    public function index(Request $request): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
-        $data = $this->repository->findAll();
+        $data = $this->repository->findAll($request->per_page ?? 10);
         return GenericResource::collection($data);
     }
 }
--- source/Infrastructure/Repository/Operations/FindAll.php
+++ source/Infrastructure/Repository/Operations/FindAll.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Collection;
 trait FindAll
 {
     /**
+     * @param int $perPage
      * @return mixed
      */
-    public function findAll(): mixed
+    public function findAll(int $perPage = 10): mixed
     {
-        return $this->model->latest()->paginate(11);
+        return $this->model->latest()->paginate($perPage);
     }
 }
